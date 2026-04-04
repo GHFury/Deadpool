300a8113a631153fe7f2205299eed2793d0f4f5a
GHFury <brianb@MSFury.localdomain>
2026-04-04 00:23:03 -0700

    fix: fix unit testing

---
+++ tests/unit/test_main.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+from app.main import app
+
+
+def test_home():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Pipeline is working!" in response.data
